e2f76da0b9f2d66daea9ef93700493fd54de89a8
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-08-30 19:01:29 +0530

    chore: add constants for assets

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+const CONSTANTS = {
+	SPELL_LOGO_BG_RED:
+		"https://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo.png",
+	LOGIN_SCREEN_BG:
+		"https://assets.nflxext.com/ffe/siteui/vlv3/00103100-5b45-4d4f-af32-342649f1bda5/64774cd8-5c3a-4823-a0bb-1610d6971bd4/IN-en-20230821-popsignuptwoweeks-perspective_alpha_website_medium.jpg",
+};
+
+export default CONSTANTS;
