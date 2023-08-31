3c2e98c2134e0db215bf33497382a051bde9feaa
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-08-31 19:30:23 +0530

    fix: :bug: fix form validation for name in signin

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -5,18 +5,26 @@ export const checkValidFormData = (name, email, password) => {
 		password: null,
 	};
 
+	// If name is undefined, it means we are in sign in mode and shouldn't check for name validation
+	const toTestName = name === undefined ? false : true;
+
 	const isNameValid = /^[a-zA-Z]+ [a-zA-Z]+$/.test(name);
 	const isEmailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
 	const isPasswordValid =
 		/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
+	// If feilds doesn't match the regex, also set the error message
 	if (!isEmailValid) errorMessage.email = "Email id is not valid!";
 	if (!isPasswordValid) errorMessage.password = "Password is not valid!";
-	if (!isNameValid) errorMessage.name = "Name is not valid!";
+	if (toTestName && !isNameValid) errorMessage.name = "Name is not valid!";
 
-	if (name.length === 0) errorMessage.name = "Name is required!";
-	if (email.length === 0) errorMessage.email = "Email id is required!";
-	if (password.length === 0) errorMessage.password = "Password is required!";
+	// If feilds are empty (overriding the regex error message in case of empty)
+	if (toTestName && (!name || name?.length === 0))
+		errorMessage.name = "Name is required!";
+	if (!email || email?.length === 0)
+		errorMessage.email = "Email id is required!";
+	if (!password || password?.length === 0)
+		errorMessage.password = "Password is required!";
 
 	return errorMessage;
 };
