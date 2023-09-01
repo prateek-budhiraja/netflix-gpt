df8be4a4d2b4a31fabbfe6aad6b13bc3db755f76
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-09-01 17:10:29 +0530

    feat: :sparkles: add profile option

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,58 @@
+import { useSelector } from "react-redux";
 import CONSTANTS from "../utils/constants";
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Header = () => {
+	const location = useLocation();
+	const navigate = useNavigate();
+	const user = useSelector((state) => state.user);
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	const handleSignout = () => {
+		signOut(auth)
+			.then(() => {
+				navigate("/");
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
 	return (
-		<div>
+		<div className="absolute top-0 left-0 right-0 z-10 p-4 flex justify-between items-center">
 			<img
 				src={CONSTANTS.SPELL_LOGO_BG_RED}
 				alt="netflix logo"
-				className="absolute w-40 top-6 left-10 z-10"
+				className="w-24 sm:w-40"
 			/>
+			{user && location.pathname !== "/" && (
+				<div>
+					<img
+						onClick={() => setIsMenuOpen(!isMenuOpen)}
+						src={CONSTANTS.USER_AVATAR}
+						alt="userpic"
+						className="w-6 sm:w-10 cursor-pointer"
+					/>
+					<div
+						className={`${
+							!isMenuOpen && "hidden"
+						} absolute bg-white rounded right-2 top-[60px] sm:top-[90px] p-2 shadow-md`}
+					>
+						<div className="w-4 h-4 bg-white rotate-45 absolute -top-2 right-2"></div>
+						<h4>Welcome! {user.displayName}</h4>
+						<hr className="mb-1 mt-2" />
+						<span
+							className="text-red-500 cursor-pointer inline-block"
+							onClick={handleSignout}
+						>
+							Sign Out
+						</span>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,6 +3,8 @@ const CONSTANTS = {
 		"https://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo.png",
 	LOGIN_SCREEN_BG:
 		"https://assets.nflxext.com/ffe/siteui/vlv3/00103100-5b45-4d4f-af32-342649f1bda5/64774cd8-5c3a-4823-a0bb-1610d6971bd4/IN-en-20230821-popsignuptwoweeks-perspective_alpha_website_medium.jpg",
+	USER_AVATAR:
+		"https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg",
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
 	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
