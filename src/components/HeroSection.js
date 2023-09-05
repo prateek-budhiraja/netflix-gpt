bfe1973d7c4f8e3d62b5567a65a9da47ce704a9f
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-09-05 23:03:07 +0530

    feat: :sparkles: create hero section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,22 +1,24 @@
-import React from "react";
 import { useSelector } from "react-redux";
 import VideoBG from "./VideoBG";
 import MovieInfo from "./MovieInfo";
 
 const HeroSection = () => {
 	const nowPlaying = useSelector((state) => state.movie.nowPlaying);
-	let randomMovie = null;
 
-	if (nowPlaying.length) {
-		randomMovie = nowPlaying[Math.floor(Math.random() * nowPlaying.length)];
-		console.log(randomMovie);
-	}
+	if (!nowPlaying.length) return null;
+
+	const randomMovie = nowPlaying[Math.floor(Math.random() * nowPlaying.length)];
+
+	console.log(nowPlaying);
+	console.log(randomMovie);
+
+	const { title, overview } = randomMovie;
 
 	return (
-		<div>
+		<>
 			<VideoBG movie={randomMovie} />
-			<MovieInfo movie={randomMovie} />
-		</div>
+			<MovieInfo title={title} overview={overview} />
+		</>
 	);
 };
 
--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -1,7 +1,19 @@
-import React from "react";
-
-const MovieInfo = () => {
-	return <div>MovieInfo</div>;
+const MovieInfo = ({ title, overview }) => {
+	return (
+		<>
+			<div className="w-full aspect-video bg-black opacity-50 absolute top-0" />
+			<div className="text-white absolute top-[50%] -translate-y-[50%] left-12">
+				<h1 className="text-6xl font-bold">{title}</h1>
+				<p className="my-4 leading-5 w-[480px]">{overview}</p>
+				<button className="bg-gray-100 text-lg font-medium text-black px-6 py-1.5 rounded-full hover:opacity-90 inline-block mr-4">
+					▶ Play
+				</button>
+				<button className="bg-white text-lg font-medium text-black px-6 py-1.5 rounded-full bg-opacity-50">
+					ℹ️ More Info
+				</button>
+			</div>
+		</>
+	);
 };
 
 export default MovieInfo;
